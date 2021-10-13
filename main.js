5e93f8d839b60889806771589e51f8760df608a4
Shubhak5 <83910260+Shubhak5@users.noreply.github.com>
2021-10-13 22:10:49 +0530

    ADV_C137_AI_ObjectFinder_Project
    Added js code to detect the object mentioned in the text box and speak the object name

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 model_status = "";
 object_tobe_detected = "";
+objects = [];
 
 function setup() {
     canvas = createCanvas(430, 330);
@@ -11,6 +12,32 @@ function setup() {
 
 function draw() {
     image(video, 0, 0, 430, 330);
+
+    if (model_status != "") {
+        objectDetector.detect(video, gotResult);
+
+        for (i = 0; i < objects.length; i++) {
+
+            if (objects[i].label == object_tobe_detected) {
+                percent = floor(objects[i].confidence * 100);
+                document.getElementById("status").innerHTML = "Status: Object Detected";
+
+                fill("#FF0000");
+                text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y);
+
+                noFill();
+                stroke("#FF0000");
+                rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+                var synth = window.speechSynthesis;
+                speak_data = objects[i].label;
+                var utterThis = new SpeechSynthesisUtterance(speak_data);
+                synth.speak(utterThis);
+            }
+        }
+    } else {
+        document.getElementById("status").innerHTML = "Status: Object Not Detected";
+    }
 }
 
 function start() {
@@ -20,7 +47,15 @@ function start() {
 }
 
 function modelLoaded() {
-
     console.log("cocossd model is Initiated!");
     model_status = true;
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(results);
+        objects = results;
+    }
 }
